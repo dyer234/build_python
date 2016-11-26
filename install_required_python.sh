92ced664df428a30d43fd1a47edc0825d2fadd84
Daniel Dyer <dyer234@gmail.com>
2016-11-25 19:00:04 -0800

    a script to download, compile and install a custom version of python

---
+++ install_required_python.sh
@@ -0,0 +1,71 @@
+
+
+PYTHON_MAJOR="3"
+PYTHON_MINOR="5"
+PYTHON_REVISION="2"
+
+VERSION="$PYTHON_MAJOR.$PYTHON_MINOR.$PYTHON_REVISION"
+
+PYTHON_NAME="Python-$VERSION"
+
+PROJECT_DIR=`pwd`
+DIRECTORY_FOR_VENV="${PROJECT_DIR}/venv"
+WORKING_DIRECTORY="${PROJECT_DIR}/WORK_TEMP"
+PYTHON_BINARY="${DIRECTORY_FOR_VENV}/bin/python"
+
+
+python_install_location="${HOME}/PythonVersions"
+
+echo "Installing Python Version:"
+echo $PYTHON_NAME
+echo ""
+
+
+download_python()
+{
+	curl -O "https://www.python.org/ftp/python/$VERSION/$PYTHON_NAME.tgz"
+}
+
+
+compile_python()
+{
+	# install dependencies
+	sudo apt-get install build-essential
+	sudo apt-get install libssl-dev openssl libsqlite3-dev
+	
+	# compile python
+	tar -xzvf "$PYTHON_NAME.tgz"
+	cd $PYTHON_NAME
+	./configure --prefix="${python_install_location}"
+
+	make
+	sudo make install
+	cd ../
+ }
+
+
+create_virtual_environment()
+{
+	# make virtualenv
+	${python_install_location}python${PYTHON_MAJOR}.${PYTHON_MINOR} -m venv "${DIRECTORY_FOR_VENV}"
+	cd "${PROJECT_DIR}"
+	source venv/bin/activate
+	ls
+	python --version
+}
+
+
+main()
+{
+	mkdir -p "${WORKING_DIRECTORY}"
+	cd "${WORKING_DIRECTORY}"
+
+	download_python
+
+	compile_python
+
+	create_virtual_environment
+}
+
+
+main
